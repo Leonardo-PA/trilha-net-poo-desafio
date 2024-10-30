49b3fe56e45d09b8423f736a9e325d661d7817b6
Leonardo Pereira de Andrade <121520088+Leonardo-PA@users.noreply.github.com>
2024-10-30 14:00:48 -0300

    Update Program.cs
    Alteração classe Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+    public static void Main(string [] arg)
+    {
+        //Objeto Nokia
+        Nokia nokia = new Nokia ("12345", "Nokia 123", "123456789IMEI", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+
+        //Objeto Iphone
+        Iphone iphone = new Iphone("67890", "Iphone XV", "1234567890IMEI", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+
+    }
+}
